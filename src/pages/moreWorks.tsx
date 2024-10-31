21970d40c3255a5fef0a9d7c1bcd9b4075aaf3ad
lolster45 <oscarneutron3@gmail.com>
2024-10-30 20:30:28 -0500

    flex layout to grid layout and style changes

--- src/pages/moreWorks.tsx
+++ src/pages/moreWorks.tsx
@@ -48,11 +48,9 @@ const moreWorks = () => {
         <section id={styles.more_work_section}>
             <div className={styles.projects}>
                 <h1>Notable Projects</h1>
-
                 <div 
                     className={styles.projects_wrapper}
                 > 
-
                     {
                         projectsMain.map(proj => {
 
@@ -69,7 +67,9 @@ const moreWorks = () => {
 
 
                             return (
-                                <div className={styles.card_proj}>
+                                <div 
+                                    className={styles.card_proj} 
+                                >
                                     <div
                                         ref={ref}
                                         className={`${styles.image_container} ${inView ? styles.active : ''}`}
@@ -81,10 +81,10 @@ const moreWorks = () => {
                                             alt="picture of my converter website project"                    
                                         />  
                                         <div className={styles.image_links}>
-                                            <a className={styles.git_link} href="" target='_blank'>
+                                            <a className={styles.git_link} href={proj.links.github} target='_blank'>
                                                 <FaGithub/>
                                             </a>
-                                            <a className={styles.live_link} href="" target='_blank'>
+                                            <a className={styles.live_link} href={proj.links.live} target='_blank'>
                                                 <MdLiveTv/>
                                             </a>               
                                         </div>  
@@ -94,90 +94,30 @@ const moreWorks = () => {
                                         className={`${styles.title} ${inView2 ? styles.active : ''}`}
                                     >
                                         <h2>{proj.title}</h2> 
+                                        <div className={styles.proj_info}>
+                                            <h3>Technologies Used:</h3>
+                                            <div className={styles.tech_wrap}>
+                                                {
+                                                    proj.icons.map(item => {
+                                                        return (
+                                                            <span>
+                                                                <Image 
+                                                                    src={item.img} 
+                                                                    alt='image of language used'
+                                                                />
+                                                                {item.name}
+                                                            </span>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
 
                             )
                         })
                     }
-
-
-
-                    {/* <div>
-                        <div className={styles.image_container}>
-                            <Image 
-                            src={Quiz}
-                            alt="picture of my quiz project website"                 
-                            />    
-                        </div>
-
-                        <h2>Quiz</h2>
-                        
-                    </div>
-                    <div>
-                        <div className={styles.image_container}>
-                            <Image 
-                            src={Calc}
-                            alt="picture of my calculator project" 
-                            />    
-                        </div>
-
-                        <h2>Calculator</h2>
-                       
-                    </div>
-                    <div>
-                        <div className={styles.image_container}>
-                            <Image 
-                                src={MarkUp}
-                                alt="picture of my markup editor website project"      
-                            />    
-                        </div>
-
-                        <h2>MarkUp Editor</h2>
-                       
-                    </div>
-                    <div>
-                        <div className={styles.image_container}>
-                            <Image 
-                            src={Ecom}
-                            alt="picture of my E commerse website project"    
-                            />    
-                        </div>
-
-                        <h2>ECOM</h2>
-                        
-                    </div>
-                    
-                    <div>
-                        <div className={styles.image_container}>
-                            <Image 
-                            src={ColorS}
-                            alt="profile picture of me"                 
-                            />    
-                        </div>
-
-                        <h2>Color Gen.</h2>
-                    </div>
-                    <div>
-                        <div className={styles.image_container}>
-                            <Image 
-                            src={TicTacToe}
-                            alt="profile picture of me" 
-                            />    
-                        </div>
-
-                        <h2>Tic-Tac-Toe</h2>
-                    </div>
-                    <div>
-                        <div className={styles.image_container}>
-                            <Image 
-                            src={PassWordGen}
-                            alt="profile picture of me"                  
-                            />    
-                        </div>
-
-                        <h2>PassWord Generator</h2>
-                    </div> */}
 
                 </div>
             </div>     
